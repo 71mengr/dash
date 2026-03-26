8afa66f89cf6a2b9dd8d42615be71f3719418ad7
kolm <hoko@tkmc.jk>
2026-03-26 11:46:09 +0000

    pow: use internal blake3 for kawheavy final mix

--- src/pow/kawheavy/kawheavy.cpp
+++ src/pow/kawheavy/kawheavy.cpp
@@ -4,55 +4,13 @@
 
 #include <pow/kawheavy/kawheavy.h>
 
+#include <crypto/kawheavy/blake3_wrapper.h>
 #include <pow/kawheavy/kawheavy_kawpow.h>
-
-#include <llvm-c-20/llvm-c/blake3.h>
 #include <uint256.h>
 
-#include <algorithm>
-#include <array>
-#include <cstddef>
-#include <dlfcn.h>
-#include <stdexcept>
-#include <mutex>
 
 namespace KAWHeavy {
 namespace {
-
-struct Blake3Api final {
-    using InitFn = void (*)(llvm_blake3_hasher*);
-    using UpdateFn = void (*)(llvm_blake3_hasher*, const void*, size_t);
-    using FinalizeFn = void (*)(const llvm_blake3_hasher*, uint8_t*, size_t);
-
-    void* handle{nullptr};
-    InitFn init{nullptr};
-    UpdateFn update{nullptr};
-    FinalizeFn finalize{nullptr};
-};
-
-const Blake3Api& GetBlake3Api()
-{
-    static Blake3Api api;
-    static std::once_flag once;
-    std::call_once(once, [] {
-        constexpr const char* candidates[] = {"libLLVM.so.20.1", "libLLVM.so.20", "libLLVM.so"};
-        for (const char* soname : candidates) {
-            api.handle = dlopen(soname, RTLD_LAZY | RTLD_LOCAL);
-            if (api.handle != nullptr) {
-                break;
-            }
-        }
-
-        if (api.handle == nullptr) {
-            return;
-        }
-
-        api.init = reinterpret_cast<Blake3Api::InitFn>(dlsym(api.handle, "llvm_blake3_hasher_init"));
-        api.update = reinterpret_cast<Blake3Api::UpdateFn>(dlsym(api.handle, "llvm_blake3_hasher_update"));
-        api.finalize = reinterpret_cast<Blake3Api::FinalizeFn>(dlsym(api.handle, "llvm_blake3_hasher_finalize"));
-    });
-    return api;
-}
 
 bool TryExternalHybridHash(const uint256& header_hash, uint32_t nonce, int32_t height, const EffectiveParams& params, uint256& out)
 {
@@ -66,20 +24,7 @@ bool TryExternalHybridHash(const uint256& header_hash, uint32_t nonce, int32_t h
 
 uint256 FinalMixWithBlake3(const uint256& kheavy_state)
 {
-    const Blake3Api& api = GetBlake3Api();
-    if (api.init == nullptr || api.update == nullptr || api.finalize == nullptr) {
-        throw std::runtime_error("BLAKE3 symbols are unavailable: llvm_blake3_hasher_init/update/finalize");
-    }
-
-    llvm_blake3_hasher hasher{};
-    std::array<uint8_t, LLVM_BLAKE3_OUT_LEN> blake3_out{};
-    api.init(&hasher);
-    api.update(&hasher, kheavy_state.begin(), uint256::size());
-    api.finalize(&hasher, blake3_out.data(), blake3_out.size());
-    uint256 out;
-    static_assert(uint256::size() == LLVM_BLAKE3_OUT_LEN, "BLAKE3 output must be 32 bytes");
-    std::copy(blake3_out.begin(), blake3_out.end(), out.begin());
-    return out;
+    return kawheavy::Blake3(MakeUCharSpan(kheavy_state));
 }
 
 } // namespace
