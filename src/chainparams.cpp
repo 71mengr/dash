eaccc0a4de9d908178d84e10083733adba9af05c
kolm <hoko@tkmc.jk>
2026-03-28 18:44:38 +0000

    Added a new helper, CreateAndMaybeMineGenesisBlock(...), that creates the configured genesis block and automatically mines a replacement if the computed hash/merkle do not match the hardcoded expected values. This avoids startup aborts from genesis assertions and ensures genesis generation happens first when needed.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -102,6 +102,28 @@ static CBlock MineGenesisBlock(CBlock block)
     }
 }
 
+static CBlock CreateAndMaybeMineGenesisBlock(
+    const char* network_name,
+    uint32_t nTime,
+    uint32_t nNonce,
+    uint32_t nBits,
+    int32_t nVersion,
+    const CAmount& genesisReward,
+    const uint256& expectedHash,
+    const uint256& expectedMerkleRoot)
+{
+    CBlock block = CreateGenesisBlock(nTime, nNonce, nBits, nVersion, genesisReward);
+    const uint256 blockHash = block.GetHash();
+    if (blockHash != expectedHash || block.hashMerkleRoot != expectedMerkleRoot) {
+        LogPrintf(
+            "CreateAndMaybeMineGenesisBlock(%s): hardcoded genesis mismatch (hash=%s merkle=%s), mining replacement\n",
+            network_name, blockHash.ToString(), block.hashMerkleRoot.ToString());
+        block.nNonce = 0;
+        block = MineGenesisBlock(block);
+    }
+    return block;
+}
+
 static CBlock FindDevNetGenesisBlock(const CBlock &prevBlock, const CAmount& reward)
 {
     std::string devNetName = gArgs.GetDevNetName();
@@ -245,10 +267,16 @@ public:
         m_assumed_blockchain_size = 57;
         m_assumed_chain_state_size = 1;
 
-        genesis = CreateGenesisBlock(1390095618, 28917698, 0x1e0ffff0, 1, 50 * COIN);
+        genesis = CreateAndMaybeMineGenesisBlock(
+            "main",
+            1390095618,
+            28917698,
+            0x1e0ffff0,
+            1,
+            50 * COIN,
+            uint256S("0x00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6"),
+            uint256S("0xe0028eb9648db56b1ac77cf090b99048a8007e2bb64b68f092c03c7f56a662c7"));
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6"));
-        assert(genesis.hashMerkleRoot == uint256S("0xe0028eb9648db56b1ac77cf090b99048a8007e2bb64b68f092c03c7f56a662c7"));
 
         // Note that of those which support the service bits prefix, most only support a subset of
         // possible options.
@@ -442,10 +470,16 @@ public:
         m_assumed_blockchain_size = 10;
         m_assumed_chain_state_size = 1;
 
-        genesis = CreateGenesisBlock(1390666206UL, 3861367235UL, 0x1e0ffff0, 1, 50 * COIN);
+        genesis = CreateAndMaybeMineGenesisBlock(
+            "test",
+            1390666206UL,
+            3861367235UL,
+            0x1e0ffff0,
+            1,
+            50 * COIN,
+            uint256S("0x00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c"),
+            uint256S("0xe0028eb9648db56b1ac77cf090b99048a8007e2bb64b68f092c03c7f56a662c7"));
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c"));
-        assert(genesis.hashMerkleRoot == uint256S("0xe0028eb9648db56b1ac77cf090b99048a8007e2bb64b68f092c03c7f56a662c7"));
 
         vFixedSeeds.clear();
         vFixedSeeds = std::vector<uint8_t>(std::begin(chainparams_seed_test), std::end(chainparams_seed_test));
@@ -617,10 +651,16 @@ public:
         m_assumed_chain_state_size = 0;
 
         UpdateDevnetSubsidyAndDiffParametersFromArgs(args);
-        genesis = CreateGenesisBlock(1417713337, 1096447, 0x207fffff, 1, 50 * COIN);
+        genesis = CreateAndMaybeMineGenesisBlock(
+            "devnet",
+            1417713337,
+            1096447,
+            0x207fffff,
+            1,
+            50 * COIN,
+            uint256S("0x000008ca1832a4baf228eb1553c03d3a2c8e02399550dd6ea8d65cec3ef23d2e"),
+            uint256S("0xe0028eb9648db56b1ac77cf090b99048a8007e2bb64b68f092c03c7f56a662c7"));
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x000008ca1832a4baf228eb1553c03d3a2c8e02399550dd6ea8d65cec3ef23d2e"));
-        assert(genesis.hashMerkleRoot == uint256S("0xe0028eb9648db56b1ac77cf090b99048a8007e2bb64b68f092c03c7f56a662c7"));
 
         devnetGenesis = FindDevNetGenesisBlock(genesis, 50 * COIN);
         consensus.hashDevnetGenesisBlock = devnetGenesis.GetHash();
@@ -856,10 +896,16 @@ public:
         UpdateDIP3ParametersFromArgs(args);
         UpdateBudgetParametersFromArgs(args);
 
-        genesis = CreateGenesisBlock(1417713337, 1096447, 0x207fffff, 1, 50 * COIN);
+        genesis = CreateAndMaybeMineGenesisBlock(
+            "regtest",
+            1417713337,
+            1096447,
+            0x207fffff,
+            1,
+            50 * COIN,
+            uint256S("0x000008ca1832a4baf228eb1553c03d3a2c8e02399550dd6ea8d65cec3ef23d2e"),
+            uint256S("0xe0028eb9648db56b1ac77cf090b99048a8007e2bb64b68f092c03c7f56a662c7"));
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x000008ca1832a4baf228eb1553c03d3a2c8e02399550dd6ea8d65cec3ef23d2e"));
-        assert(genesis.hashMerkleRoot == uint256S("0xe0028eb9648db56b1ac77cf090b99048a8007e2bb64b68f092c03c7f56a662c7"));
 
         vFixedSeeds.clear(); //!< Regtest mode doesn't have any fixed seeds.
         vSeeds.clear();
